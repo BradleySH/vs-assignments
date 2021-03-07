0fa1f9fd6ec2955e1ab03d5e169c3d4239bedeb8
Bradley Horlander <bradley.s.horlander@gmail.com>
2021-03-07 00:05:17 -0600

    new svae for assignemt loops and array

---
+++ loops-array-practice/index.js
@@ -0,0 +1,9 @@
+
+ const officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"];
+
+for(let i = 0; i <= officeItems.length; i++){
+    if(officeItems === [3]){
+        console.log(i);
+    }
+};
+
